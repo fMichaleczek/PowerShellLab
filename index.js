b993a656074145abaa4d1dc9cf3df45b6b437d0e
Flavien Michaleczek <fmichaleczek@gmail.com>
2020-04-27 02:19:45 +0200

    --

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 define(() => {
-    document.body.innerHTML = "<div style='width:80%;padding-left:10%;'>PowerShell WASM Started:<br><textarea id='code' rows='5' cols='50'></textarea><br><button id='submitCode' type='button'>Submit Code</button><br><textarea id='output' rows='20' cols='50'></textarea><br><textarea id='outputerror' rows='10' cols='50'></textarea></div>";
+    document.body.innerHTML = "<div style='width:80%;padding-left:10%;'>PowerShell WASM Started:<br><textarea id='code' rows='5' cols='50'>$PSVersionTable</textarea><br><button id='submitCode' type='button'>Submit Code</button><br><textarea id='output' rows='20' cols='50'></textarea><br><textarea id='outputerror' rows='10' cols='50'></textarea></div>";
 });
 
 const invokeCode = Module.mono_bind_static_method("[powershell-wasm] Microsoft.PowerShell.Wasm.Program:InvokeCode");
